6d8525e9b3253358f74e172b48f81e846383105e
sylvaus <pierreyves.breches74@gmail.com>
2019-04-07 18:47:42 -0400

    Added license and improved setpu.py

---
+++ appify/__init__.py
@@ -0,0 +1,4 @@
+__version__ = "0.1.0"
+__version__date__ = "2019-04-07"
+
+__all__ = ["common", "cli", "ui", ]
--- setup.py
+++ setup.py
@@ -1,12 +1,25 @@
 from setuptools import setup
 
+import appify
+
+with open("readme.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
-    name='appify',
-    version='',
-    packages=['appify'],
-    url='',
-    license='',
-    author='sylvaus',
-    author_email='',
-    description='Helpers to transform functions into CLI or simple UIs'
+    name="appify",
+    version=appify.__version__,
+    packages=["appify"],
+    url="https://github.com/sylvaus/appify",
+    license="MIT",
+    author="sylvaus",
+    author_email="",
+    description="Helpers to transform functions into CLI or simple UIs",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
 )
