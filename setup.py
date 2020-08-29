08298c0a67e9bd1e89f9b67212e05398f825522a
sylvaus <pierreyves.breches74@gmail.com>
2020-08-29 11:18:11 -0400

    Improved setup.py: version retrieval + extra_requires for testing

--- setup.py
+++ setup.py
@@ -1,16 +1,27 @@
+import codecs
+import os.path
+
 from setuptools import setup, find_packages
 
-import appify
+
+def read(rel_path):
+    here = os.path.abspath(os.path.dirname(__file__))
+    with codecs.open(os.path.join(here, rel_path), 'r') as fp:
+        return fp.read()
 
 
-def read(path):
-    with open(path, "r") as fh:
-        return fh.read()
+def get_version(rel_path):
+    for line in read(rel_path).splitlines():
+        if line.startswith('__version__'):
+            delim = '"' if '"' in line else "'"
+            return line.split(delim)[1]
+    else:
+        raise RuntimeError("Unable to find version string.")
 
 
 setup(
     name="appify",
-    version=appify.__version__,
+    version=get_version("appify/__init__.py"),
     packages=find_packages(),
     url="https://github.com/sylvaus/appify",
     license="MIT",
@@ -19,6 +30,9 @@ setup(
     description="Helpers to transform functions into CLI or simple UIs",
     long_description=read("readme.md"),
     long_description_content_type="text/markdown",
+    extras_require={
+        "test": ["pytest>=5.4.2", "pytest-cov>=2.9.0", "tox>=3.15.1"],
+    },
     classifiers=[
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
